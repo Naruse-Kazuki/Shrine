a9e135721bc4000299cdb4a1189bcefaceb1179c
Naruse-Kazuki <kuroraido@icloud.com>
2020-04-13 21:58:45 +0900

    shrineLogo

--- mdc_100_series/lib/main.dart
+++ mdc_100_series/lib/main.dart
@@ -13,7 +13,35 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
-
 import 'package:Shrine/app.dart';
 
 void main() => runApp(ShrineApp());
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset("assets/diamond.png"),
+                SizedBox(height: 16.0),
+                Text("SHRINE"),
+              ],
+            ),
+            SizedBox(height: 120.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
